ea0937252e0d9292b8500ba1858e113252996126
Luis Sanchez <luis.udec.sanchez@hotmail.com>
2024-02-08 21:23:04 -0500

    fix: "Rename type of User"

--- src/hooks/useSignIn.ts
+++ src/hooks/useSignIn.ts
@@ -1,9 +1,9 @@
 import { API_BASE_URL } from "../constants";
-import { User } from "../types";
+import type { PostUserResponse } from "../types";
 import { useEffect, useState } from "react";
 
 export const useSignIn = () => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<PostUserResponse | null>(null);
   const [status, setStatus] = useState("idle");
 
   useEffect(() => {
@@ -11,7 +11,7 @@ export const useSignIn = () => {
       setStatus("loading");
       try {
         const response = await fetch(`${API_BASE_URL}/api/users/sign-in`);
-        const data: User = await response.json();
+        const data: PostUserResponse = await response.json();
         setUser(data);
         setStatus("success");
       } catch (error) {
